3ca0facfa6b7fc13f05bcc54dd2679c128d27e28
ferarochadev <fer.arocha@hotmail.com>
2024-10-16 14:18:22 -0300

    entrega de projeto

--- index.py
+++ index.py
@@ -1,2 +1,35 @@
-nome = "Fernando"
-print('nome')
+
+heroi = str(input('Digite o nome do herói: '))
+
+
+while True:
+    
+    xp = int(input('Digite a quantidade de xp do herói: '))
+     
+    if xp < 1000:
+        nivel = 'Ferro'
+        break
+    elif 1000 <= xp < 2000:
+        nivel = 'Bronze'
+        break
+    elif 2000 <= xp < 5000:
+        nivel = 'Prata'
+        break
+    elif 5000 <= xp < 6000:
+        nivel = 'Ouro'
+        break
+    elif 6000 <= xp < 7000:
+        nivel = 'Platina'
+        break
+    elif 7000 <= xp < 8000:
+        nivel = 'Ascendente'
+        break
+    elif 8000 <= xp < 9000:
+        nivel = 'Imortal'
+        break
+    else:
+        nivel = 'Radiante'
+        break
+        
+print(f'O herói de nome {heroi} está no nível de {nivel}.')
+
